9241f79db1b0411b7006c4b898e92daef59fec4d
Creeperuuu <gigabajen@gmail.com>
2026-08-22 10:08:09 +0800

    Adapt shader loader mixin for Sodium 0.9.1

--- src/client/java/de/linusdev/sodiumcoreshadersupport/mixin/client/MixinShaderLoader.java
+++ src/client/java/de/linusdev/sodiumcoreshadersupport/mixin/client/MixinShaderLoader.java
@@ -1,6 +1,5 @@
 package de.linusdev.sodiumcoreshadersupport.mixin.client;
 
-
 import net.caffeinemc.mods.sodium.client.gl.shader.GlShader;
 import net.caffeinemc.mods.sodium.client.gl.shader.ShaderConstants;
 import net.caffeinemc.mods.sodium.client.gl.shader.ShaderLoader;
@@ -27,54 +26,50 @@ public class MixinShaderLoader {
     private static void loadShaderInject(
             ShaderType type, Identifier name, ShaderConstants constants, CallbackInfoReturnable<GlShader> cir
     ) {
-        LOG.info("Start loading shader in namespace '"  + name.getNamespace() + "': " + name.getPath());
+        LOG.info("Start loading shader in namespace '{}': {}", name.getNamespace(), name.getPath());
     }
 
     /**
+     * Loads Sodium shader sources from the active Minecraft ResourceManager instead of
+     * directly from the Sodium classpath resources.
+     *
      * @author LinusDev
-     * @reason Load shaders from resources, loaded by then ResourceManager instead of reading them as java resource.
+     * @reason Allow resource packs to override Sodium core shaders.
      */
     @Overwrite
     public static String getShaderSource(Identifier name) {
-
-        if(shaders == null) {
-            LOG.warn("Trying to load shaders, but shaders variable not yet initialised");
-            // fallback to default getShaderSource
+        if (shaders == null) {
+            LOG.warn("Trying to load shaders, but shaders variable is not yet initialised; falling back to Sodium's classpath loader");
             String path = String.format("/assets/%s/shaders/%s", name.getNamespace(), name.getPath());
 
             try (InputStream in = ShaderLoader.class.getResourceAsStream(path)) {
                 if (in == null) {
                     throw new RuntimeException("Shader not found: " + path);
-                } else {
-                    return IOUtils.toString(in, StandardCharsets.UTF_8);
                 }
+                return IOUtils.toString(in, StandardCharsets.UTF_8);
             } catch (IOException e) {
                 throw new RuntimeException("Failed to read shader source for " + path, e);
             }
         }
 
-        var nameSpace = shaders.get(name.getNamespace());
+        var namespace = shaders.get(name.getNamespace());
+        if (namespace == null) {
+            throw new RuntimeException("No shaders available for namespace '" + name.getNamespace() + "'");
+        }
 
-        if(nameSpace == null)
-            throw new RuntimeException("No Shaders available for namespace '" + name.getNamespace() + "'");
-
-        var shaderResource = nameSpace.get(name.getPath());
-
-        if(shaderResource == null)
-            throw new RuntimeException("No Shader found in namespace '" + name.getNamespace()
+        var shaderResource = namespace.get(name.getPath());
+        if (shaderResource == null) {
+            throw new RuntimeException("No shader found in namespace '" + name.getNamespace()
                     + "' for shader '" + name.getPath() + "'");
+        }
 
         try {
-
-            //noinspection resource: This would close the pack which is not what we want.
-            LOG.info("Loaded Shader '{}:{}' from pack '{}'.", name.getNamespace(), name.getPath(), shaderResource.source().location().title().getString());
-
-
+            LOG.info("Loaded shader '{}:{}' from pack '{}'.", name.getNamespace(), name.getPath(),
+                    shaderResource.source().location().title().getString());
             return IOUtils.toString(shaderResource.open(), StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new RuntimeException("Exception while reading shader source in namespace '" + name.getNamespace()
                     + "' for shader '" + name.getPath() + "'", e);
         }
     }
-
 }
